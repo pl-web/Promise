7e873d590c84cae60efebc530f9d31a856fd9183
it-reader <hnature@126.com>
2021-07-02 00:06:16 +0800

    promise demo

---
+++ 02/promise.js
@@ -0,0 +1,11 @@
+// Promise
+// 1.为一个构造函数
+// 2.封装一个异步操作并返回其执行的结果
+
+// promise status
+// pending
+// resolved
+// rejected
+// pending => resolved   return value
+// pending => rejected   return reason
+
